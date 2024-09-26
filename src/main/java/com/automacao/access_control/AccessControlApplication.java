1b70dd99a49de3a7ca02a2b0c2715c4c13dfe7f6
paulo_mendonca <paulo.amadeu18@gmail.com>
2024-09-26 10:12:42 -0400

    refactor class main name

--- src/main/java/com/automacao/access_control/AccessControlApplication.java
+++ src/main/java/com/automacao/access_control/AccessControlApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AcessControlApplication {
+public class AccessControlApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AcessControlApplication.class, args);
+		SpringApplication.run(AccessControlApplication.class, args);
 	}
 
 }
